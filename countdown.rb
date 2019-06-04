fd968e1375a96123e357b1f85d24c7a2eb95e3a7
David Foote <david.a.foote@gmail.com>
2019-06-04 02:23:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  counter = 9
+  while counter < 10
+  puts "#{x} SECOND(S)!"
+  counter -= 1
+  end
+  puts "HAPPY NEW YEAR!"
 end
+
+countdown(10)
+
+def count_down_with_sleep(y)
+  counter = 5
+  while counter < 5
+  puts "#{y} SECOND(S)!"
+  counter -= 1
+  sleep(1.0)
+  end 
+end
+
+count_down_with_sleep(5)
